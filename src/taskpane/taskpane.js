b739e4a85b08c6041286310b8c41c56bf567c853
yoshiki <yoshiki.tajima@jp.ricoh.com>
2022-06-03 09:03:08 +0900

    initial commit

---
+++ src/taskpane/taskpane.js
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global document, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Outlook) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    var app_body = document.getElementById("app-body");
+    app_body.children[0].innerHTML = "<b>Get Message</b> <br/>";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+export async function run() {
+  // Get a reference to the current message
+  var item = Office.context.mailbox.item;
+  item.body.getAsync(
+    "text",
+    { asyncContext: "This is passed to the callback" },
+    function callback(result) {
+      // Write message property value to the task pane
+      document.getElementById("item-subject").innerHTML = "<b>Message:</b> <br/>" + result.value;
+    });
+}
